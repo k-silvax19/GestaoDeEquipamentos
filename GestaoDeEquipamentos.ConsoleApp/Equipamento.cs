c5955571818e52aaff2afa23c878bdc7f330582a
Kauan Silva <kauanmacedo338@gmail.com>
2026-04-12 16:23:48 -0300

    Adicionado, Cadastrar Ferramenta, Editar Ferramenta e excluir ferramenta

---
+++ GestaoDeEquipamentos.ConsoleApp/Equipamento.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace GestaoDeEquipamentos.ConsoleApp;
+
+public class Equipamento
+{
+    public string id;
+    public string nome;
+    public string fabricante;
+    public decimal precoAquisicao;
+    public DateTime dataFabricacao;
+}
